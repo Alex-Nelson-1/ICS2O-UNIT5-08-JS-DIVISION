f52fb0395f70d79d772f152a72679ad1d402ec44
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-12-08 19:45:47 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-UNIT5-08-JS-WHILE-LOOPS-DIVISION/sw.js", {
+    scope: "/ICS2O-UNIT5-08-JS-WHILE-LOOPS-DIVISION/",
   })
 }
 
