5d0e863c957987d3c463975cb0cdc00913c2ef47
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-12-17 17:46:17 +0000

    Division function + comments

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Created by: Alex Nelson
-// Created on: Oct 2022
+// Created on: Dec 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -8,17 +8,32 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register(
-    "/ICS2O-UNIT5-08-JS-WHILE-LOOPS-DIVISION/sw.js",
-    {
-      scope: "/ICS2O-UNIT5-08-JS-WHILE-LOOPS-DIVISION/",
-    }
-  )
+  navigator.serviceWorker.register("/ICS2O-UNIT5-08-JS-WHILE-LOOPS-DIVISION/sw.js", {
+    scope: "/ICS2O-UNIT5-08-JS-WHILE-LOOPS-DIVISION/",
+  })
 }
 
 /**
- * This function displays an alert.
+ * This function does basic division using loops.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function multiply() { 
+  //Variable that gets divided
+  let numberOne = parseFloat(document.getElementById("numberOne").value)
+  //Variable that is dividing
+  let numberTwo = parseFloat(document.getElementById("numberTwo").value)
+  //Output variable
+  let quotient = null
+  //redo statement
+  if (numberTwo > numberOne || numberOne < 0 || numberTwo < 0) {
+  document.getElementById("redo").innerHTML = "Please reload the page. A number is negative or number 2 is larger than number 1."
+  }
+  else {
+    //division feature
+  while (numberTwo <= numberOne) {
+    numberOne = numberOne - numberTwo
+    quotient = quotient + 1
+    }
+  }
+  //Output
+document.getElementById("product").innerHTML = quotient + " R " + numberOne
 }
